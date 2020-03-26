282c80fb8b23f23342fbc86691640c30b95e1de4
Fredrick Widjaya <f.widjaya20@gmail.com>
2020-03-26 16:41:07 +0700

    adding calculation logic (normal way)

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 import java.util.Vector;
 
+import cards.Bronze;
 import cards.Card;
+import cards.Gold;
+import cards.Silver;
+import products.Flight;
 import products.Food;
+import products.Hotel;
 import products.Product;
 
 public class Main {
 
 	public static void main(String[] args) {
+		double totalPrice = 0;
 		Vector<Product> cart = new Vector<Product>();
 		
 		cart.add(new Food("Wiskas", 5500));
 		cart.add(new Food("Lays", 7500));
 		
-		Card myCard = null;
+		Card myCard = new Silver();
 		
 		// TODO: Calculate Price + Discount
 		/**
@@ -33,8 +39,63 @@ public class Main {
 		 */
 		
 		for (Product item : cart) {
-			System.out.println(item.toString());
+			System.out.print(item.toString());
+			
+			if (myCard == null) {
+				totalPrice += item.price;
+				System.out.println("");
+			} else
+			
+			if (myCard instanceof Bronze) {
+				double discountPrice = 0;
+				if (item instanceof Food) {
+					discountPrice = item.price * 0.05;
+				} else	
+				if (item instanceof Flight) {
+					discountPrice = item.price * 0.10;
+				} else
+				if (item instanceof Hotel) {
+					discountPrice = item.price * 0.15;
+				}
+				
+				totalPrice += item.price - discountPrice;
+				System.out.println(" - Discount : Rp. " + discountPrice);
+			} else
+			
+			if (myCard instanceof Silver) {
+				double discountPrice = 0;
+				if (item instanceof Food) {
+					discountPrice = item.price * 0.15;
+				} else	
+				if (item instanceof Flight) {
+					discountPrice = item.price * 0.20;
+				} else
+				if (item instanceof Hotel) {
+					discountPrice = item.price * 0.25;
+				}
+				
+				totalPrice += item.price - discountPrice;
+				System.out.println(" - Discount : Rp. " + discountPrice);
+			} else
+			
+			if (myCard instanceof Gold) {
+				double discountPrice = 0;
+				if (item instanceof Food) {
+					discountPrice = item.price * 0.50;
+				} else	
+				if (item instanceof Flight) {
+					discountPrice = item.price * 0.50;
+				} else
+				if (item instanceof Hotel) {
+					discountPrice = item.price * 0.50;
+				}
+				
+				totalPrice += item.price - discountPrice;
+				System.out.println(" - Discount : Rp. " + discountPrice);
+			}
 		}
+		
+		System.out.println("Total Price : Rp. " + totalPrice);
 	}
 	
 }
